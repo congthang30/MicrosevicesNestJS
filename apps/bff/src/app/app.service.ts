0bd748d539b53cd1bb8f94e298ac8883b45ade8b
Cong Thang <23dh113302@st.huflit.edu.vn>
2025-12-04 14:07:35 +0700

    test

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { PORT } from '@common/constants/common.constant'
+import { PORT } from '@common/constants/common.constant';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    console.log('PORT',PORT);
+    console.log('PORT', PORT);
     return { message: 'Hello API' };
   }
 }
